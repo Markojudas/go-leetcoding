a20a9e1744fa4c02c638d81ca8fec0638db78ae3
Jose Hernandez <josrhernandez1985@gmail.com>
2022-06-25 22:28:49 -0400

    extra: palindrome

--- extra/palindrome/main.go
+++ extra/palindrome/main.go
@@ -14,22 +14,22 @@ func isPalindrome(str string) bool {
 		leftChar := int32(str[leftPtr])
 		rightChar := int32(str[rightPtr])
 
-		if unicode.IsPunct(leftChar) || unicode.IsSpace(leftChar) {
+		for unicode.IsPunct(leftChar) || unicode.IsSpace(leftChar) {
 			leftPtr++
 			leftChar = int32(str[leftPtr])
-		} else {
-			if unicode.IsUpper(leftChar) {
-				leftChar = leftChar + 32
-			}
 		}
 
-		if unicode.IsPunct(rightChar) || unicode.IsSpace(rightChar) {
+		if unicode.IsUpper(leftChar) {
+			leftChar = leftChar + 32
+		}
+
+		for unicode.IsPunct(rightChar) || unicode.IsSpace(rightChar) {
 			rightPtr--
 			rightChar = int32(str[rightPtr])
-		} else {
-			if unicode.IsUpper(rightChar) {
-				rightChar = rightChar + 32
-			}
+		}
+
+		if unicode.IsUpper(rightChar) {
+			rightChar = rightChar + 32
 		}
 
 		if leftChar != rightChar {
@@ -39,11 +39,12 @@ func isPalindrome(str string) bool {
 		leftPtr++
 		rightPtr--
 	}
+
 	return true
 }
 
 func main() {
 
-	fmt.Println(isPalindrome("Race car!"))
+	fmt.Println(isPalindrome("Race car!!!!!"))
 	fmt.Println(isPalindrome("Hello"))
 }
