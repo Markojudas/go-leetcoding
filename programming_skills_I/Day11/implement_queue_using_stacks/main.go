3a4106743e5c863e4517a2fd2d244377d65dbb5e
Jose Hernandez <josrhernandez1985@gmail.com>
2022-06-28 12:02:08 -0400

    finished 3/4 of Day11

---
+++ programming_skills_I/Day11/implement_queue_using_stacks/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+type MyQueue struct {
+	list []int
+}
+
+func Constructor() MyQueue {
+
+	mq := MyQueue{
+		list: []int{},
+	}
+
+	return mq
+}
+
+func (mq *MyQueue) Push(x int) {
+
+	mq.list = append(mq.list, x)
+}
+
+func (mq *MyQueue) pop() int {
+
+	elem := mq.Peek()
+
+	if len(mq.list) > 1 {
+		mq.list = mq.list[1:]
+	} else {
+		mq.list = []int{}
+	}
+	return elem
+}
+
+func (mq *MyQueue) Peek() int {
+
+	return mq.list[0]
+}
+
+func (mq *MyQueue) Empty() bool {
+
+	return len(mq.list) == 0
+}
+
+func main() {
+	obj := Constructor()
+	obj.Push(1) //null
+	obj.Push(2) // null
+
+	fmt.Println(obj.Peek())  //1
+	fmt.Println(obj.pop())   //1
+	fmt.Println(obj.Empty()) //false
+}
